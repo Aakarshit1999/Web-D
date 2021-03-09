2be0947b28c40676d43ab36b5895a228176a88c6
Aakarshit1999 <aakarshit6143@gmail.com>
2021-03-10 00:58:12 +0530

    new commit

--- simple counter/script.js
+++ simple counter/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var counter = document.querySelector(".counter")
 var follower = document.querySelector(".followers")
 let count =1;
@@ -10,17 +9,4 @@ setInterval(() => {
 }, 0.1);
 setTimeout(() => {
     follower.innerText = "followers on instagram";
-=======
-var counter = document.querySelector(".counter")
-var follower = document.querySelector(".followers")
-let count =1;
-setInterval(() => {
-    if(count < 1000){
-        count=count+3;
-        counter.innerText = count;
-    }
-}, 0.1);
-setTimeout(() => {
-    follower.innerText = "followers on instagram";
->>>>>>> de2270bc2d0f98b924b529c2760419c30187f1a3
 }, 1500);
