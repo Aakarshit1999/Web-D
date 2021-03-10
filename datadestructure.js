8204d0bbcba72c40e491ad401f2ca54884538614
Aakarshit1999 <aakarshit6143@gmail.com>
2021-03-11 01:08:12 +0530

    new file

---
+++ datadestructure.js
@@ -0,0 +1,22 @@
+// Destructuring of data is not only possible on objects but on arrays as well
+// The only thing to remember is that the name of objects remain same while destructuring using objects whereas 
+// it is not necessary to do that in array as they work in a different way but it is recommended to do the same everywhere
+
+const user = ["hitesh", 3, "admin"];
+var role = user[2];
+var name = user[0];
+
+var [name, courseCount, role] = user;
+
+console.log(role);
+
+const MyUser = {
+  Name: "hitesh",
+  CourseCount: 5,
+  Role: "admin",
+};
+
+console.log(MyUser.CourseCount);
+
+const { Name, CourseCount, Role } = MyUser;
+console.log(Role);
